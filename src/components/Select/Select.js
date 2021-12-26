898b907ce9e213afca9d832ebf59e207e34411ec
Alistair <alistairgarioch@gmail.com>
2021-12-26 18:06:24 +0000

    Feat(select): add select component to storybook

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -3,16 +3,59 @@ import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
 import Icon from '../Icon';
+import VisuallyHidden from '../VisuallyHidden'
 import { getDisplayedValue } from './Select.helpers';
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <>
+    {label && <VisuallyHidden as={'label'}> {label}</VisuallyHidden>}
+    <Wrapper >
+      {displayedValue}
+      <select value={value} onChange={onChange}>
+        {children}
+      </select>
+      <IconWrapper>
+        <Icon id='chevron-down' size={20} strokeWidth={2}/>
+      </IconWrapper>
+    </Wrapper>
+    </>
   );
 };
+
+const Wrapper = styled.div`
+  align-items: center;
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  color: ${COLORS.gray700};
+  display: flex;
+  padding: 12px 16px;
+  position: relative;
+  width: fit-content;
+  & > select {
+    opacity: 0;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 6px;
+    margin: auto;
+  }
+  &:focus-within{
+    outline: 5px auto Highlight;
+    outline: 5px auto -webkit-focus-ring-color;  }
+  &:hover {
+    color: ${COLORS.black};
+  }
+`
+const IconWrapper = styled.div` 
+  cursor: none;
+  margin-left: 12px;
+  margin-right: -6px;
+`
+
+
+
 
 export default Select;
